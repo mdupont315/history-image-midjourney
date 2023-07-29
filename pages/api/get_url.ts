92ac034a630b104a18f396664b65ff35584027e7
Senguly <135636323+Senguly@users.noreply.github.com>
2023-07-29 06:27:25 +0200

    Update get_url.ts

--- pages/api/get_url.ts
+++ pages/api/get_url.ts
@@ -1,11 +1,9 @@
 import { Client, Snowflake } from 'discord.js'
-export default async function handler() {console.log("here")
-    // const client = new Client({intents: [Snowflake.Guilds]});
+export default async function handler() {
+    const client = new Client({intents: [Snowflake.Guilds]});
 
-    // /* discord client */
-    // client.on('ready', () => {
-    //     console.log(`Logged in as ${client.user?.tag}`);
-    // });
-    // console.log("client")
-    // client.login('MTA0ODYzMjcxNTA4NTM1MzA1MA.G0xH1D.vU_r7OdwhrWGYzBilkM7-GgoLWN6lhDQMkR_SU');
+    client.on('ready', () => {
+        console.log(`Logged in as ${client.user?.tag}`);
+    });
+    client.login('');
 }
